ec22437a3dea327534b0c97836e0fa2eb49b7ac3
Pavel Selivanov <selivan.at.github@gmail.com>
2020-11-12 06:07:23 +0300

    WIP

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='test-gh-actions',
+    packages=['test-gh-actions'],
+    version=open('VERSION', 'r').read(),
+    description='test-gh-actions',
+    license='WTFPL',
+    python_requires='>=3.5',
+    install_requires=[],
+    data_files=[('',['VERSION'])],
+    scripts=[],
+)
---
+++ test-gh-actions/test-gh-actions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    args_parser=ArgumentParser(description='test github-actions')
+    args_parser.add_argument('--print', help='print this argument', default=None)
+    args = args_parser.parse_args()
+    if args.print is not None:
+        print(args.print)
